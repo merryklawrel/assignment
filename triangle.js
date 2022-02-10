23cbb028bdfc22903db2584f4c7fe124904485c0
merryklawrel <merrylawrel@gmail.com>
2022-02-10 16:12:31 +0530

    committed

--- triangle.js
+++ triangle.js
@@ -1,10 +1,10 @@
-<script>
-	function triangle(b,h){
+
+let b=4,h=8;
+console.log(h,b);
+let area= triangle(b,h);
+console.log(area);
+
+function triangle(b,h){
 	  let a=((b*h)/2);
 	  return a;
 	}
-
-	let b=4,h=6;
-	let area= triangle(b,h);
-	console.log(area);
-</script>
